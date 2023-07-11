b7a2b8ef17056e80f2a15eb0d173f7e00e94b22b
UserName <rin3810400h@gmail.com>
2023-07-11 17:22:48 +0900

    first commit

---
+++ src/main/java/com/example/EquipmentManagementAPI/EquipmentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EquipmentManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EquipmentManagementAPI/EquipmentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EquipmentManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
