c7bdd5a5b2b47182165abf06a93e4ee8ddd335a8
UserName <rin3810400h@gmail.com>
2023-07-14 17:23:52 +0900

    結合テストの残りを実装

--- src/test/java/com/example/equipment/integrationtest/EquipmentManagementApiIntegrationTest.java
+++ src/test/java/com/example/equipment/integrationtest/EquipmentManagementApiIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.equipment.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.Customization;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -126,6 +128,128 @@ public class EquipmentManagementApiIntegrationTest {
   void 指定したIDの設備が存在しない時に例外がスローされること() throws Exception {
     String response =
         mockMvc.perform(MockMvcRequestBuilders.get("/equipments/4"))
+            .andExpect(MockMvcResultMatchers.status().isNotFound())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    JSONAssert.assertEquals("""
+        {
+        "timestamp": "2023-07-14T12:00:00.511021+09:00[Asia/Tokyo]",
+        "status": "404",
+        "error": "Not Found",
+        "message": "Not Found",
+        "path": "/equipments/4"
+        }
+        """, response, new CustomComparator(JSONCompareMode.STRICT,
+        new Customization("timestamp", ((o1, o2) -> true))));
+  }
+
+  // POSTメソッドで正しくリクエストした時に、設備が登録できステータスコード201とメッセージが返されること
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/insert_equipment.yml", ignoreCols = "equipment_id")
+  @Transactional
+  void 設備が登録できること() throws Exception {
+    String response =
+        mockMvc.perform(MockMvcRequestBuilders.post("/equipments")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content("""
+                {
+                "name": "真空ポンプB",
+                "number": "A1-C001B",
+                "location": "Area1"
+                }                                
+                """))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    JSONAssert.assertEquals("""
+        {
+        "message": "設備が正常に登録されました"
+        }
+        """, response, JSONCompareMode.STRICT);
+  }
+
+  // PATCHメソッドで存在する設備IDを指定し正しくリクエストした時に、設備が更新できステータスコード200とメッセージが返されること
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/update_equipment.yml")
+  @Transactional
+  void 設備が更新できること() throws Exception {
+    String response =
+        mockMvc.perform(MockMvcRequestBuilders.patch("/equipments/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content("""
+                {
+                "name": "真空ポンプB",
+                "number": "A1-C001B",
+                "location": "Area1"
+                }                                
+                """))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    JSONAssert.assertEquals("""
+        {
+        "message": "設備が正常に更新されました"
+        }
+        """, response, JSONCompareMode.STRICT);
+  }
+
+  // PATCHメソッドで存在しない設備IDを指定した時に、例外がスローされステータスコード404とエラーメッセージが返されること
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @Transactional
+  void 更新リクエストで指定したIDの設備が存在しない時に例外がスローされること() throws Exception {
+    String response =
+        mockMvc.perform(MockMvcRequestBuilders.patch("/equipments/4")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content("""
+                {
+                "name": "真空ポンプB",
+                "number": "A1-C001B",
+                "location": "Area1"
+                }                                
+                """))
+            .andExpect(MockMvcResultMatchers.status().isNotFound())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    JSONAssert.assertEquals("""
+        {
+        "timestamp": "2023-07-14T12:00:00.511021+09:00[Asia/Tokyo]",
+        "status": "404",
+        "error": "Not Found",
+        "message": "Not Found",
+        "path": "/equipments/4"
+        }
+        """, response, new CustomComparator(JSONCompareMode.STRICT,
+        new Customization("timestamp", ((o1, o2) -> true))));
+  }
+
+  // DELETEメソッドで存在する設備IDを指定した時に、設備が削除できステータスコード200とメッセージが返されること
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/delete_equipment.yml")
+  @Transactional
+  void 設備が削除できること() throws Exception {
+    String response =
+        mockMvc.perform(MockMvcRequestBuilders.delete("/equipments/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+    JSONAssert.assertEquals("""
+        {
+        "message": "設備が正常に削除されました"
+        }
+        """, response, JSONCompareMode.STRICT);
+  }
+
+  // DELETEメソッドで存在しない設備IDを指定した時に、例外がスローされステータスコード404とエラーメッセージが返されること
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @Transactional
+  void 削除リクエストで指定したIDの設備が存在しない時に例外がスローされること() throws Exception {
+    String response =
+        mockMvc.perform(MockMvcRequestBuilders.delete("/equipments/4"))
             .andExpect(MockMvcResultMatchers.status().isNotFound())
             .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
