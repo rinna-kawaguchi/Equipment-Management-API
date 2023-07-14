57ce1d84c2934fe872a3fdba96b188719c3f5c1f
UserName <rin3810400h@gmail.com>
2023-07-14 15:52:55 +0900

    mapperクラスの残りの単体テストを実装

--- src/test/java/com/example/equipment/mapper/EquipmentMapperTest.java
+++ src/test/java/com/example/equipment/mapper/EquipmentMapperTest.java
@@ -2,6 +2,7 @@ package com.example.equipment.mapper;
 
 import com.example.equipment.entity.Equipment;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -18,11 +19,11 @@ class EquipmentMapperTest {
   @Autowired
   EquipmentMapper equipmentMapper;
 
-  // name,number,locationのいずれもクエリパラメータを入力しない場合、設備が全数検索されることのテスト
+  // 設備検索でname,number,locationに文字列を指定しない場合、設備が全数返されることのテスト
   @Test
   @DataSet(value = "datasets/equipments.yml")
   @Transactional
-  void 設備の全数検索ができること() {
+  void 全ての設備を取得できること() {
     assertThat(equipmentMapper.findEquipment(null, null, null)).hasSize(3).contains(
         new Equipment(1, "真空ポンプA", "A1-C001A", "Area1"),
         new Equipment(2, "吸込ポンプB", "A2-C002B", "Area2"),
@@ -30,4 +31,61 @@ class EquipmentMapperTest {
     );
   }
 
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @Transactional
+  void name_number_locationに指定した内容と部分一致する設備が取得できること() {
+    assertThat(equipmentMapper.findEquipment("ポンプ", "C00", "1")).hasSize(1).contains(
+        new Equipment(1, "真空ポンプA", "A1-C001A", "Area1")
+    );
+  }
+
+  @Test
+  @DataSet(value = "datasets/empty.yml")
+  @Transactional
+  void 設備が無い時に空のListが返されること() {
+    assertThat(equipmentMapper.findEquipment(null, null, null)).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @Transactional
+  void 指定したIDの設備が取得できること() {
+    assertThat(equipmentMapper.findEquipmentById(1)).contains(
+        new Equipment(1, "真空ポンプA", "A1-C001A", "Area1")
+    );
+  }
+
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @Transactional
+  void 指定したIDの設備が存在しない時に空のOptionalが返されること() {
+    assertThat(equipmentMapper.findEquipmentById(4)).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/insert_equipment.yml", ignoreCols = "equipment_id")
+  @Transactional
+  void 設備登録ができ既存のIDより大きい数字のIDが採番されること() {
+    Equipment equipment = new Equipment("真空ポンプB", "A1-C001B", "Area1");
+    equipmentMapper.insertEquipment(equipment);
+    assertThat(equipment.getEquipmentId()).isGreaterThan(3);
+  }
+
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/update_equipment.yml")
+  @Transactional
+  void 指定したIDの設備が更新できること() {
+    equipmentMapper.updateEquipment(1, "真空ポンプB", "A1-C001B", "Area1");
+  }
+
+  @Test
+  @DataSet(value = "datasets/equipments.yml")
+  @ExpectedDataSet(value = "datasets/delete_equipment.yml")
+  @Transactional
+  void 指定したIDの設備が削除できること() {
+    equipmentMapper.deleteEquipment(1);
+  }
 }
