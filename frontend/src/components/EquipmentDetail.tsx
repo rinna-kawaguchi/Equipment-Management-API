bc55743e8dba142a6e2130e30478da35a8950196
UserName <rin3810400h@gmail.com>
2023-07-20 17:57:05 +0900

    設備詳細ページの点検計画部分を実装

--- frontend/src/components/EquipmentDetail.tsx
+++ frontend/src/components/EquipmentDetail.tsx
@@ -1,13 +1,22 @@
-import { Divider, HStack, Heading, Input } from "@chakra-ui/react"
+import { Divider, HStack, Heading, Input, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
 import { BaseButton } from "./atoms/BaseButton"
 import { useNavigate, useParams } from "react-router-dom";
 import { FC, useEffect, useState } from "react";
 import { Equipment } from "./FindEquipment";
 import axios from "axios";
 
+type Plan = {
+  checkPlanId: number;
+  equipmentId: number;
+  checkType: string;
+  period: string;
+  deadline: string;
+}
+
 export const EquipmentDetail: FC = () => {
   const [selectedEquipment, setSelectedEquipment] = useState<Equipment | null>(null);
   const [deleteMessage, setDeleteMessage] = useState("")
+  const [selectedEquipmentPlans, setSelectedEquipmentPlans] = useState<Array<Plan>>([]);
 
   const { id } = useParams();
 
@@ -26,6 +35,10 @@ export const EquipmentDetail: FC = () => {
     axios.get<Equipment>(`http://localhost:8080/equipments/${id}`).then((res) => setSelectedEquipment(res.data))
   }, [])
 
+  useEffect(() => {
+    axios.get<Array<Plan>>(`http://localhost:8080/equipments/${id}/plans`).then((res) => setSelectedEquipmentPlans(res.data))
+  }, [])
+
   return (
     <div>
       <Heading>設備情報詳細</Heading>
@@ -41,6 +54,29 @@ export const EquipmentDetail: FC = () => {
         <Input value={selectedEquipment?.location} width={"400px"} backgroundColor={"gray.100"} placeholder="設置場所" />
       </HStack>
       <br />
+      <br />
+      <Heading size={"md"}>点検計画</Heading>
+      <Divider my={3} />
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>点検種別</Th>
+              <Th>点検周期</Th>
+              <Th>点検期限</Th>
+            </Tr>
+          </Thead>
+          {selectedEquipmentPlans.map((plan) => (
+            <Tbody>
+              <Tr key={plan.checkPlanId}>
+                <Td>{plan.checkType}</Td>
+                <Td>{plan.period}</Td>
+                <Td>{plan.deadline}</Td>
+              </Tr>
+            </Tbody>
+          ))}
+        </Table>
+      </TableContainer>
       <br />
       <BaseButton onClick={onClickBackFindPage}>戻る</BaseButton>
       <BaseButton onClick={onClickUpdatePage}>修正</BaseButton>
