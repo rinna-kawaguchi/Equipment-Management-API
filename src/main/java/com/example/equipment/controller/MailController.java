230854134ee38b32345ac401d81a04f65c347896
UserName <rin3810400h@gmail.com>
2023-08-02 15:55:22 +0900

    メール送付するメソッドが1日おきに実行されるよう設定

--- src/main/java/com/example/equipment/controller/MailController.java
+++ src/main/java/com/example/equipment/controller/MailController.java
@@ -1,27 +1,29 @@
 package com.example.equipment.controller;
 
 import com.example.equipment.service.MailService;
-import org.springframework.mail.MailSender;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
 
+@Configuration
+@EnableScheduling
 @RestController
 public class MailController {
 
-  private final MailSender mailSender;
   private final MailService mailService;
 
-  public MailController(MailSender mailSender, MailService mailService) {
-    this.mailSender = mailSender;
+  public MailController(MailService mailService) {
     this.mailService = mailService;
   }
 
+  // 1日おきに実行する
+  @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
   @GetMapping("/mail")
-  public String write1() throws ParseException {
-
+  public void write1() throws ParseException {
     mailService.mailSend();
-    return "hello";
   }
 }
