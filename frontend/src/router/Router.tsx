e45a5adcfd88dabe04bd69a06ed2b1876d8de6ea
UserName <rin3810400h@gmail.com>
2023-08-02 17:26:42 +0900

    ヘッダーを作成

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -4,13 +4,14 @@ import { CreateEquipment } from '../components/pages/CreateEquipment';
 import { EquipmentDetail } from '../components/pages/EquipmentDetail';
 import { FC } from 'react';
 import { NotFound } from '../components/pages/NotFound';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
 
 export const Router: FC = () => {
   return (
     <Routes>
-      <Route path='/find' element={<FindEquipment />} />
-      <Route path='/create' element={<CreateEquipment />} />
-      <Route path='/update/:id' element={<EquipmentDetail />} />
+      <Route path='/find' element={<HeaderLayout><FindEquipment /></HeaderLayout>} />
+      <Route path='/create' element={<HeaderLayout><CreateEquipment /></HeaderLayout>} />
+      <Route path='/update/:id' element={<HeaderLayout><EquipmentDetail /></HeaderLayout>} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
